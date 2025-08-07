b4fc1312d2809989ad91e740b4fce65c2a3c109f
karimNafiz <nafiz.rim@gmail.com>
2025-08-07 19:56:36 -0400

    created the SingletonMonobehavior class so that other singletons can inherit from this instead of the boiler plate of creating a new singleton

---
+++ Assets/Scripts/Utility/Singleton/SingletonMonoBehavior.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+namespace Utility.Singleton
+{
+    public class SingletonMonoBehavior<T> : MonoBehaviour where T : SingletonMonoBehavior<T>
+    {
+        static T instance;
+        public static T Instance => instance;
+            
+
+        private void Awake()
+        {
+            if (instance != null && instance != this) 
+            {
+                Destroy(this.gameObject);
+                return;
+            }
+            instance = (T)this;
+
+        }
+
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
