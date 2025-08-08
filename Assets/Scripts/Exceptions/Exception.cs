716e0741c1719c37824288bafbfaea60a9f5e6b5
Nafiz Karim <nafiz.rim@gmail.com>
2025-08-08 19:28:43 -0400

    created the Exceptions namespace and created the onSuccessCallBack

---
+++ Assets/Scripts/Exceptions/Exception.cs
@@ -0,0 +1,17 @@
+using System;
+
+
+namespace Exceptions 
+{ 
+
+
+
+    public class BadRequestException : Exception 
+    {
+        public BadRequestException(string message) : base(message) { }
+
+
+    }
+
+
+}
--- Assets/Scripts/WebServer/WebServerClient.cs
+++ Assets/Scripts/WebServer/WebServerClient.cs
@@ -3,6 +3,7 @@ using Utility.Singleton;
 using System.Collections.Generic;
 using Models.Bots;
 using Utility.HTTP;
+using Exceptions;
 namespace WebServer
 {
     public class WebServerClient : SingletonMonoBehavior<WebServerClient>
@@ -20,11 +21,37 @@ namespace WebServer
 
             Action<string> onSuccCallback = new Action<string>((string json)=> 
             {
-                List<Dictionary<string, object>> dictFrmJson = HTTPClient.Instance.ParseJsonToDictionaryList(json);
-                
+                try
+                {
+                    List<Dictionary<string, object>> dictFrmJson = HTTPClient.Instance.ParseJsonToDictionaryList(json);
+                    // important note, the reason im making this hashSet is so that we can do Set Operations on these bots
+                    HashSet<Bot> bots = new HashSet<Bot>();
+                    foreach (Dictionary<string, object> pair in dictFrmJson)
+                    {
 
+                        bots.Add(GetBotFrmJson(pair));
+
+
+                    }
+                    onSuccess?.Invoke(bots);
+                }
+                catch (BadRequestException ex)
+                {
+                    // do something
+                }
+                catch (Exception ex) 
+                { 
+                    // do something
+                }
+            });
+
+
+            Action<string> onErrCallback = new Action<string>((string err)=> 
+            { 
             
-            }); 
+                
+            
+            });
 
 
             HTTPClient.Instance.GetRequest($"{url.GetHostUrl()}{endpoint}")
@@ -32,17 +59,37 @@ namespace WebServer
 
         }
 
-
+        // IMPORTANT TODO: refactor all this bullshit code to it's dedicated file if we are expanding this project
         public Bot GetBotFrmJson(Dictionary<string, object> dict) 
         {
-            if (!dict.ContainsKey("name")) 
+            
+            if (!dict.ContainsKey("id")) throw new BadRequestException("json for bot is missing the key id ");        
+            if (!dict.ContainsKey("name")) throw new BadRequestException("json for bot is missing the key name ");        
+            if (!dict.ContainsKey("location")) throw new BadRequestException("json for bot is missing the key location ");        
+            if (!dict.ContainsKey("age")) throw new BadRequestException("json for bot is missing the key age ");        
+            if (!dict.ContainsKey("status")) throw new BadRequestException("json for bot is missing the key status ");
+            try
+            {
+                return new Bot(dict["name"].ToString(), dict["location"].ToString(), int.Parse(dict["age"].ToString()), int.Parse(dict["status"].ToString()), int.Parse(dict["id"].ToString()));
+
+            }
+            catch (FormatException ex) 
             {
                 
-                throw new Exception();
+                throw ex;
+            }
+            catch (InvalidCastException ex)
+            {
+                throw ex;
+            }
+            catch (OverflowException ex) 
+            {
+                throw ex;
             }
         
         
         }
+        
 
 
 
