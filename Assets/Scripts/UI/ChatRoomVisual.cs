f8f1871090c35e44e9e06595000c12c1967ad835
Nafiz Karim <nafiz.rim@gmail.com>
2025-08-10 16:30:45 -0400

    added functionality for text input

--- Assets/Scripts/UI/ChatRoomVisual.cs
+++ Assets/Scripts/UI/ChatRoomVisual.cs
@@ -12,14 +12,27 @@ using System;
     don't judge my ass based on this code
  
  */
+/*
+    current plan of action 
+    we need a script for the TextInputVisual
+    we neet to listen to its event OnTextInput -> this event is run when the submit button is run
+    we get the string from the OnTextInput
+    we add it to the MessageContainer
+    
+ */
 
 
 public class ChatRoomVisual : SingletonMonoBehavior<ChatRoomVisual>
 {
    
-    // have a script for this
+    // this will handle all the message visual pop ups
     [SerializeField] private MessageVisualManager messageContainer;
+    // this handles getting new text input from the user
+    [SerializeField] private TextInputVisual textInput;
+
+    // this represents the current chat bot that we are speaking to 
     private ChatBotVisual currentChatBotVisual;
+    // we use this to keep track of which chat bot the player is trying to speak
     private EventBinding<OnChatBotVisualClicked> eventBinding_OnChatBotVisualClicked;
     private void Awake()
     {
@@ -27,12 +40,26 @@ public class ChatRoomVisual : SingletonMonoBehavior<ChatRoomVisual>
         eventBinding_OnChatBotVisualClicked = new EventBinding<OnChatBotVisualClicked>(EventBus_OnChatBotVisualClicked);
         EventBus<OnChatBotVisualClicked>.Register(eventBinding_OnChatBotVisualClicked);
     }
-    
+    private void Start()
+    {
+        textInput.OnTextEnter += TextInput_OnTextEnter;
+    }
+
+    private void TextInput_OnTextEnter(object sender, string e)
+    {
+        // take the text, and add it to the message container
+        messageContainer.AddMessageVisualUser(e);
+        // I need to send this information back to the server 
+      
+    }
+
 
     // TODO try to remove http logic from this class mainly concerned UI
     private void EventBus_OnChatBotVisualClicked(OnChatBotVisualClicked eventArgs) 
     {
         if (currentChatBotVisual != null && currentChatBotVisual == eventArgs._chatBotVisual) return;
+        // we first clear everything
+        messageContainer.Clear();
         // we need to do a web request
         Debug.Log($"getting the messaeges for bot id {eventArgs._chatBotVisual.Bot.ID}");
         WebServerClient.Instance.GetMessages(GlobalConfigs.Instance.GetServerUrl(), GlobalConfigs.Instance.globalConstant.message_endpoint_get, eventArgs._chatBotVisual.Bot, OnGetMessageSuccess, OnGetMessageErr);
