014d7b03ef06de7b98c6a02b901ceb4d5f3a23a4
Nafiz Karim <nafiz.rim@gmail.com>
2025-08-11 02:47:04 -0400

    set up the input reader for FirstPersonMode

---
+++ Assets/Scripts/InputActions/InputReader.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using System;
+namespace InputActons
+{
+    public class InputReader : MonoBehaviour
+    {
+        private PlayerInputActions playerInputActions;
+        public event Action<Vector2> OnPlayerMove_FirstPersonMode;
+        public event Action<Vector2> OnPlayerLook_FirstPersonMode;
+        public event Action<bool> OnPlayerInteract_FirstPersonMode;
+
+        // maybe we can keep track of the game state and according to the game state we can change the bindings
+
+
+        private void Awake()
+        {
+            // create a new instance of the class
+            playerInputActions = new PlayerInputActions();
+            // TODO: later on when we introduce more stuff, we take care of enabling which action map to use
+            playerInputActions.FirstPersonMovement.Enable();
+            playerInputActions.FirstPersonMovement.Movement.performed += MovementPerformed_FirstPersonMode;
+            playerInputActions.FirstPersonMovement.Looking.performed += LookingPeformed_FirstPersonMode;
+            playerInputActions.FirstPersonMovement.Interaction.performed += InteractonPerformed_FirstPersonMode;
+
+
+        }
+
+        private void InteractonPerformed_FirstPersonMode(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+        {
+            OnPlayerInteract_FirstPersonMode?.Invoke(obj.ReadValue<bool>());
+        }
+
+        private void LookingPeformed_FirstPersonMode(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+        {
+            OnPlayerLook_FirstPersonMode?.Invoke(obj.ReadValue<Vector2>());
+        }
+
+        private void MovementPerformed_FirstPersonMode(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+        {
+            OnPlayerMove_FirstPersonMode?.Invoke(obj.ReadValue<Vector2>());
+        }
+    }
+}
