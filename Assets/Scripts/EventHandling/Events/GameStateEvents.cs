36151c620752bd0787280379ea13c84e778fa22c
Nafiz Karim <nafiz.rim@gmail.com>
2025-08-12 00:23:06 -0400

    created the game state manager, the EGameStates enum and the OnGameStateEvent

---
+++ Assets/Scripts/EventHandling/Events/GameStateEvents.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using GameStates;
+namespace EventHandling.Events
+{
+
+    
+
+    public class OnGameStateChange : IEvent 
+    {
+        public EGameState _gameState;
+    
+    }
+
+    public class GameStateEvents
+    {
+
+
+    }
+
+
+
+}
---
+++ Assets/Scripts/GameState/GameStateManager.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using Utility.Singleton;
+using EventHandling;
+using EventHandling.Events;
+namespace GameStates
+{
+    public enum EGameState
+    {
+        FIRSTPERSONMOVEMENT,
+        INTERACTION,
+        COMPUTERINTERACTION,
+
+    }
+
+
+    public class GameStateManager : SingletonMonoBehavior<GameStateManager>
+    {
+        private EGameState currentGameState;
+        [SerializeField] private EGameState gameStartState = EGameState.FIRSTPERSONMOVEMENT;
+
+
+        protected override void Awake()
+        {
+            base.Awake();
+            
+            currentGameState = gameStartState;
+            EventBus<OnGameStateChange>.Raise(new OnGameStateChange()
+            {
+
+                _gameState = gameStartState
+            }) ;
+        }
+
+    }
+}
